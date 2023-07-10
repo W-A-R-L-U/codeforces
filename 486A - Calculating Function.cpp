574cdb4ed52da10e12aaa85bc21e52b736412037
Naalla Venkateshwarlu <114277372+W-A-R-L-U@users.noreply.github.com>
2023-07-10 21:25:45 +0530

    Create 486A - Calculating Function.cpp

---
+++ 486A - Calculating Function.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long num;
+    cin>>num;
+    if(num%2==0) cout<<num/2;
+    else cout<<num/2-num;
+    cout<<endl;
+    return 0;
+}
